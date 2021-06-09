a67e3e5362484f90396abbcbd2720509b5861402
kraglik <kraglik@tochka.com>
2021-06-09 18:36:08 +0500

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+import os.path
+
+readme = ''
+here = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(here, 'README.md')
+if os.path.exists(readme_path):
+    with open(readme_path, 'rb') as stream:
+        readme = stream.read().decode('utf8')
+
+
+setup(
+    long_description=readme,
+    name='ore-combinators',
+    version='0.0.1',
+    description='Parser combinator library for Python',
+    python_requires='==3.*,>=3.6.0',
+    project_urls={'homepage': 'https://github.com/kraglik/ore', 'repository': 'https://github.com/kraglik/ore'},
+    author='Igor Kraglik',
+    author_email='kraglik.i.d@gmail.com',
+    license='MIT',
+    keywords='parser combinator ore',
+    packages=['ore'],
+    package_data={},
+    install_requires=[],
+    extras_require={},
+)
